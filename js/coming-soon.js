3a92bbb3ed730d2d330e0213e4b74644ccd8b9ee
Riccardo De Martis <riccardo@demartis.it>
2020-09-12 19:55:18 +0200

    feat: lazy loading for video

--- js/coming-soon.js
+++ js/coming-soon.js
@@ -3,4 +3,16 @@
 
   // No JS
 
+  // $("body > video").removeAttr('preload');
+  // $("body > video")[0].play();
+
+  $(window).resize(function() {
+    if(window.innerWidth >=768){
+      $("body > video").get(0).play();
+    }else{
+      $("body > video").get(0).pause();
+    }
+
+  }).resize();
+
 })(jQuery); // End of use strict
